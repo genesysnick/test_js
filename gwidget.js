fade765a6348eb8cac0fb48a3f5039eb02e03cf7
genesysnick <113726634+genesysnick@users.noreply.github.com>
2024-06-03 11:43:48 -0500

    Create gwidget.js

---
+++ gwidget.js
@@ -0,0 +1,12 @@
+  (function (g, e, n, es, ys) {
+    g['_genesysJs'] = e;
+    g[e] = g[e] || function () {
+      (g[e].q = g[e].q || []).push(arguments)
+    };
+    g[e].t = 1 * new Date();
+    g[e].c = es;
+    ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+  })(window, 'Genesys', 'https://apps.mypurecloud.com/genesys-bootstrap/genesys.min.js', {
+    environment: 'prod',
+    deploymentId: '99eb299b-7246-426b-b08f-9c6bb5501570'
+  });
